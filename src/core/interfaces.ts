f919da0ff4e5e0ebe6d3a50a3385cee5591e02e6
Sn0wFox <ruben.pericas@laposte.net>
2016-04-11 20:55:14 +0200

    Updated User

--- src/core/interfaces.ts
+++ src/core/interfaces.ts
@@ -1,6 +1,6 @@
 /***************************************************************
  ***************************************************************
- *                    OMNICHAT - Interfaces
+ *          OMNICHAT - Interfaces
  *
  *  Some parts of this documents will probably become a more
  *  official wiki later on. That's why it's written mostly in
@@ -17,20 +17,20 @@ import * as Promise from 'bluebird';
 /***************************************************************
  * Client is the entry point for the library.
  * Maintains the list of available proxies and connected users.
- * TODO : is last point still right ?
+ * TODO : is last point still right ? If not, remove it
  ***************************************************************/
 export class Client{
   proxies: Proxy[];   // Les proxys disponibles pour ce client
-  users: User[];      // Les utilisateurs connectes a ce client
+  users: User[];    // Les utilisateurs connectes a ce client
 
 	// Retourne le premier proxy permettant d'utiliser
 	// le protocole "protocol"
   getProxyFor(protocol: string): Promise<Proxy> {
-    for(let i=0; i<this.proxies.length; i++){
-      if(this.proxies[i].isCompatibleWith(protocol)){
-        return Promise.resolve(this.proxies[i]);
-      }
+  for(let i=0; i<this.proxies.length; i++){
+    if(this.proxies[i].isCompatibleWith(protocol)){
+    return Promise.resolve(this.proxies[i]);
     }
+  }
   }
 }
 
@@ -57,16 +57,16 @@ export interface Proxy{
 	//  Envoie le message "msg" dans la discussion "discussion"
 }
 
-// A NOTER :    On pourrait implementer uniquement certaines methodes en faisant
-//              une implementation implicite : on ne declare pas la classe comme
-//              "implements ...", mais on redefinie uniquement les methodes qui nous
-//              interessent. Le compilo TypeScript comprendra alors que la classe
-//              implemente tout de meme l'interface en question.
-//              Ce fonctionnement est toutefois DANGEREUX : que se passe-t-il si
-//              on tente d'appeler une methode de maniere polymorphe (par exemple
-//              une methode statique sur l'un des constructeurs du tableau proxies
-//              de la classe Client, mais que celle-ci n'est pas defini pour la classe
-//              en question) ?
+// A NOTER :  On pourrait implementer uniquement certaines methodes en faisant
+//        une implementation implicite : on ne declare pas la classe comme
+//        "implements ...", mais on redefinie uniquement les methodes qui nous
+//        interessent. Le compilo TypeScript comprendra alors que la classe
+//        implemente tout de meme l'interface en question.
+//        Ce fonctionnement est toutefois DANGEREUX : que se passe-t-il si
+//        on tente d'appeler une methode de maniere polymorphe (par exemple
+//        une methode statique sur l'un des constructeurs du tableau proxies
+//        de la classe Client, mais que celle-ci n'est pas defini pour la classe
+//        en question) ?
 
 
 /***************************************************************
@@ -74,12 +74,9 @@ export interface Proxy{
  * A contact may be the same for differents accounts. That's why
  * it contains a list of accounts : those were the contact is
  * identified as the same as in the others.
- ***************************************************************/
-/***************************************************************
- * La seule maniere de discuter avec un contact et de commencer
- * une discussion avec lui : creer un objet Discussion.
- * Des participants a la discussion pourront etre ajoutes via
- * l'interface de Discussion.
+ * The only way to tchat with someone is to start a discussion
+ * with him. Other participants coul be added through the
+ * interface Discussion.
  ***************************************************************/
 export interface Contact{
   accounts: Account[];  //  La liste des comptes connus de l'utilisateur
@@ -96,8 +93,8 @@ export interface Contact{
 												//  et à identifier clairement un contact
 
   getAccounts(): Promise<Account[]>;
-  //    Retourne la liste des comptes connus de l'utilisateur
-  //    pour lesquels ce contact est le meme.
+  //  Retourne la liste des comptes connus de l'utilisateur
+  //  pour lesquels ce contact est le meme.
 
 	mergeContacts(contact: Contact, callback?: (err: Error, succes: any) => any): void;
   // Fusionne les comptes du contact courant avec ceux du contact fourni
@@ -120,42 +117,52 @@ export interface Contact{
  * User is the representation of someone connected with OmniChat.
  * An user works quite like a Contact : you just have more
  * rights as an user (for example acceed to your own contacts).
- * So User will probably inherit from Contact.
+ * So User will probably inherit from Contact later.
+ * TODO: inherit or not ? If yes, what is the purpose of merge ?
  ***************************************************************/
-/***************************************************************
- * Seul point a noter : le fonctionnement recursif de
- * getAccounts(). Voir plus bas.
- * Attention si on venait a faire heriter User the Contact.
- * Cela serait certes pratique, mais bien que les deux classes
- * partageront a priori des attributs communs, leurs
- * significations et utilisations pourront differer.
- ***************************************************************/
-export interface User{
-  startDiscussion(contacts: Contact[]) : Discussion;
-  //    Permet de commencer une discussion avec un contact.
-  //    C'est le seul moyen de communiquer avec quelqu'un.
-  // Garanti que l'iniateur de la conversation est présent
+export interface User {
+	accounts: Account[];  //  La liste des comptes connus de l'utilisateur
 
-  leaveDiscussion(discussion: Discussion): Promise<any>;
+	username: string;     //  Le nom complet de l'utilisateur
 
-  getAccounts(): Account[];
-  //    Retourne la liste des comptes de l'utilisateurs.
-  //    Ce comptes peuvent bien entendu etre de tout type :
-  //    IRC, Skype, Facebook... mais aussi OmniChat (recursivite).
-  //    Probablement une surcharge de celle de Contact.
+  getOrCreateDiscussion(contacts: Contact[]) : Promise<Discussion>;
+  //  Permet de commencer une discussion avec un contact,
+	//  ou de recuperer une discussion existante.
+  //  C'est le seul moyen de communiquer avec quelqu'un.
+  //  En cas de création, garanti que l'initiateur de la
+	//  conversation est présent en tant que participant.
 
-  getContacts(): Contact[];
-  //    Retourne la liste des contacts de l'utilisateur courant.
-  //    Fera appel a StaticProxy.getContacts(a: Accounts) pour
-  //    chaque compte de lie a l'utiliateur.
+  leaveDiscussion(discussion: Discussion, callback?: (err: Error, succes: any) => any): void;
+	//  Permet de quitter la discussion "discussion" et de ne plus
+	//  recevoir les notifications associées.
+
+  getAccounts(): Promise<Account[]>;
+  //  Retourne la liste des comptes de l'utilisateurs.
+  //  Ce comptes peuvent bien entendu etre de tout type :
+  //  IRC, Skype, Facebook... mais aussi OmniChat (recursivite).
+  //  Probablement une surcharge de celle de Contact.
+
+  getContacts(): Promise<Contact[]>;
+  //  Retourne la liste des contacts de l'utilisateur courant.
+  //  Pour chaque compte lie a l'utilisateur,
+
+	addAccount(account: Account): Promise<any>;
+	// Ajoute un compte a l'utilisateur courant
+
+	removeAccount(accout: Account): Promise<any>;
+	// Supprime un compte de l'utilisateur courant
 
   addContact(contact: Contact): Promise<any>;
+	// Ajoute un contact a l'utilisateur courant
 
   removeContact(contact: Contact): Promise<any>;
+	// Supprime un contact de l'utilisateur courant
 
   onDiscussionRequest(callback: (disc:Discussion) => any): User;
+	// TODO : a quoi sert le retour ?
 
   onContactRequest(callback: (contact: Contact) => any): User;
+	// TODO : a quoi sert le retour ?
 }
 
 /***************************************************************
@@ -199,14 +206,14 @@ export interface DiscussionAuthorization{
 
 export interface Discussion{
   creationDate: Date;   // Date de creation de la conversatio
-  name: String;         // Nom de la conversation
+  name: String;     // Nom de la conversation
   isPrivate: boolean;
 
   getMessages(): Message[];
-  //    Retourne une liste des messages echanges pendant la discussion.
-  //    Equivalent a un espece de getHistory {dateMax, dateMin, nbMessages}.
-  //    Un filtre pourra certainement etre applique par la suite,
-  //    via utilisation de lambdas ou/et de hasmaps.
+  //  Retourne une liste des messages echanges pendant la discussion.
+  //  Equivalent a un espece de getHistory {dateMax, dateMin, nbMessages}.
+  //  Un filtre pourra certainement etre applique par la suite,
+  //  via utilisation de lambdas ou/et de hasmaps.
 
   // TODO: add filter
   // ex:
@@ -215,38 +222,38 @@ export interface Discussion{
   //  accounts: Account | Account[]
   //}
   sendMessage(author: Account, msg: Message): Promise<Message>;
-  //    Envoie le message à tous les participants de la discussion.
-  //    Devra sans doute faire appel a StaticProxy.sendMessage().
-  //    Mais un probleme se pose : sur quel compte du contact envoyer
-  //    le message ?
+  //  Envoie le message à tous les participants de la discussion.
+  //  Devra sans doute faire appel a StaticProxy.sendMessage().
+  //  Mais un probleme se pose : sur quel compte du contact envoyer
+  //  le message ?
   sendText(author: Account, text: string): Promise<Message>;
 
   addParticipants(p: Contact[]): Promise<any>;
-  //    Ajoute des participants a la conversation.
-  //    Peut etre n'ajouter qu'un seul compte des contacts,
-  //    et transformer la discussion en tant qu'agregation
-  //    de comptes et non plus de Contacts.
+  //  Ajoute des participants a la conversation.
+  //  Peut etre n'ajouter qu'un seul compte des contacts,
+  //  et transformer la discussion en tant qu'agregation
+  //  de comptes et non plus de Contacts.
 
   getParticipants(): Contact[];
-  //    Retourne une liste des participants a la conversation.
+  //  Retourne une liste des participants a la conversation.
 
   onMessage(callback: (msg: Message) => any): Discussion;
-  //    Appelle la methode a executer lors de la reception du message.
-  //    Le detection d'un message se fera sans doute via un ecouteur
-  //    qui ecoutera... quoi ?
+  //  Appelle la methode a executer lors de la reception du message.
+  //  Le detection d'un message se fera sans doute via un ecouteur
+  //  qui ecoutera... quoi ?
 
   getName(): string;
-  //    Retourne le nom de la discussion.
+  //  Retourne le nom de la discussion.
 
   getDescription(): string;
-  //    Retourne une description de la discussion.
-  //    Reste a definir ce que doit etre cette description.
+  //  Retourne une description de la discussion.
+  //  Reste a definir ce que doit etre cette description.
 
   getSettings(): any;
-  //    Retourne tout les paramètres de la discussion, même spécifiques (map).
-  //    Bien evidemment, nous ne pourrons pas tout traiter.
-  //    Nous essayerons cependant de faire du mieux possible sans pour autant
-  //    y passer des heures entieres.
+  //  Retourne tout les paramètres de la discussion, même spécifiques (map).
+  //  Bien evidemment, nous ne pourrons pas tout traiter.
+  //  Nous essayerons cependant de faire du mieux possible sans pour autant
+  //  y passer des heures entieres.
 }
 
 /***************************************************************
@@ -263,26 +270,26 @@ export interface Discussion{
  ***************************************************************/
 export interface Message{
   getText(): string;
-  //    Retourne une representation du message sous forme de String.
-  //    Tout message (texte, image, autre fichier) doit avoir cette
-  //    représentation pour toujours avoir quelque chose à afficher
-  //    (erreur de chargement, etc).
+  //  Retourne une representation du message sous forme de String.
+  //  Tout message (texte, image, autre fichier) doit avoir cette
+  //  représentation pour toujours avoir quelque chose à afficher
+  //  (erreur de chargement, etc).
 
   getCreationDate(): Date;
-  //    Retourne la date de creation du message.
+  //  Retourne la date de creation du message.
 
   getLastUpdateDate(): Date;
-  //    Retourne la date de derniere modification du message.
-  //    Cela ne vaut bien sur que si les messages sont editables,
-  //    ce qui conduira peut-etre a supprimer cette methode
-  //    de l'interface globale.
+  //  Retourne la date de derniere modification du message.
+  //  Cela ne vaut bien sur que si les messages sont editables,
+  //  ce qui conduira peut-etre a supprimer cette methode
+  //  de l'interface globale.
 
   getAuthor(): Account;
-  //    Retourne l'auteur du message.
+  //  Retourne l'auteur du message.
 
   getContent(): any;
-  //    Renvoi un contenu plus pertinent.
-  //    Chaque type de message devra implementer elle-meme cette methode.
+  //  Renvoi un contenu plus pertinent.
+  //  Chaque type de message devra implementer elle-meme cette methode.
 }
 
 /***************************************************************
@@ -295,11 +302,11 @@ export interface Message{
  * Quelle methodes pourront etre appelees sur un compte ?
  ***************************************************************/
 export interface Account{
-  protocols: string;    //  Une representation du protocole de communication
-                        //  utilise par ce compte.
-                        //  Protocol sera peut-etre encapsule dans une enum ou une struct
-                        //  par la suite.
-  data: any;            //  Les donnees du comptes. A definir
+  protocols: string;  //  Une representation du protocole de communication
+            //  utilise par ce compte.
+            //  Protocol sera peut-etre encapsule dans une enum ou une struct
+            //  par la suite.
+  data: any;      //  Les donnees du comptes. A definir
 
   createDiscussion(name: string): Promise<Discussion>;
 }
