0ebc65080caf1905b0e5314c1855d884bd5282c9
Sn0wFox <ruben.pericas@laposte.net>
2016-04-22 11:35:01 +0200

    Updated interface Discussion

--- src/interfaces/discussion.ts
+++ src/interfaces/discussion.ts
@@ -1,81 +1,78 @@
 import * as Bluebird from "bluebird";
 import {User} from "./user";
-import {GroupAccount} from "palantiri-interfaces";
+import {ContactAccount} from "./contact-account";
+import {GroupChat} from "./group-chat";
+import {Subdiscussion} from "./group-chat";
 import {Message} from "palantiri-interfaces";
-import {ContactAccount} from "palantiri-interfaces";
-import {utils} from "palantiri-interfaces";
 
 /***************************************************************
- * Discussion is the only thing you can use to chat with someone.
- * It provides you methods to send a message, add and remove
- * participants, and so on.
+ * Discussion is the only thing you can use to chat with
+ * someone. It provides you methods to send a message, add and
+ * remove participants, and so on.
+ * It is basically constituted of several smaller Discussions,
+ * knows as GroupChat.
+ * Discussion is multi-protocols and multi-accounts. This means
+ * that you can send the same message to several Contacts which
+ * are reachable from as many accounts as you want, using as
+ * many protocols as you want.
  ***************************************************************/
 export interface Discussion {
-  creationDate: Date;             // Date de creation de la conversation
+  creationDate: Date;             // Date de creation de la Discussion.
 
-  name: string;                   // Nom de la conversation
+  name: string;                   // Nom de la Discussion.
 
-  description: string;            // Une description brève de la discussion,
+  description: string;            // Une description brève de la Discussion,
                                   // sous forme textuelle.
 
-  heterogeneous: boolean;         // Est vrai si la discussion est heterogene,
-                                  // c'est-a-dire composee de plusieurs participants
-                                  // utilisant des protocoles differents.
+  heterogeneous: boolean;         // Est vrai si la Discussion est heterogene,
+                                  // c'est-a-dire composee de plusieurs sous
+                                  // discussions utilisant des protocoles differents.
 
-  isPrivate: boolean;             // Privacite de la conversation
+  isPrivate: boolean;             // Privacite de la Discussion.
 
-  participants: GroupAccount[];   // Liste des participants a la conversation.
-                                  // L'utilisateur n'en fait pas partie.
+  subdiscussions: Subdiscussion;  // Ensemble des conversations mono-protocole,
+																	// mono-compte dont est compose la Discussion.
+																	// La date permet une recuperation correcte des
+																	// messages de la Discussion.
 
   owner: User;                    // L'utilisateur d'Omni-Chat qui utilise
-                                  // cette discussion.
-
-  settings: utils.Dictionary<any>;// La liste des autres parametres de la discussion,
-		                              // meme specifiques.
-		                              // Cela permet aux implementations de travailler
-																	// avec plus de donnees.
+                                  // cette Discussion.
 
   getMessages(maxMessages: number, afterDate?: Date, filter?: (msg: Message) => boolean): Bluebird.Thenable<Message[]>;
-  //  Retourne une liste des maxMessages derniers messages echanges pendant la discussion,
+  //  Retourne une liste des "maxMessages" derniers messages echanges pendant la Discussion,
   //  au plus : s'il y en a moins, alors retourne le nombre de messages disponibles.
   //  Si afterDate est precise, ne retourne que les messages posterieurs a afterDate.
   //  Si filter est precise, ne retourne que les messages dont l'application de la fonction
   //  filter ne retourne true.
+	//  Il est a noter qu'aucun message anterieur a la date d'ajout de chaque subdiscussion
+	//  ne sera recupere, cela n'ayant aucune signification pour nous.
 
-  sendMessage(msg: Message, callback?: (err: Error, succes: Message) => any): Bluebird.Thenable<Discussion>;
-  //  Envoie le message "msg" a tous les participants de la discussion.
-  //  Il est a noter que le message ne pourra etre envoye que si
-	//  les comptes de l'utilisateurs necessaires a cet envoie
-	//  disposent tous d'une connection allumee.
-
-  addParticipants(p: GroupAccount): Bluebird.Thenable<Discussion>;
-  //  Ajoute les membres de "p" a la discussion courante.
-  //  Ces participants peuvent aussi bien etre un groupe
-  //  (deja existants ou non) qu'une personne seule.
-  //  Si il existe deja un GroupAccount utilisant le meme
-  //  protocole, p sera ajoute a ce groupe.
+  addSubdiscussion(subdiscuss: GroupChat): Bluebird.Thenable<Discussion>;
+  //  Ajoute la sous discussion "subdiscuss" a la Discussion courante.
+  //  Si il existe deja un GroupChat utilisant le meme protocole et
+	//  le meme compte, "subdiscuss" sera ajoute a ce groupe.
+	//  Si la sous discussion n'exsite pas, elle sera creee. Et si le
+	//  prtocole utilise le permet, les membres de la sous discussion
+	//  en seront informes. Sinon, ils ne le seront que lors de l'envoie
+	//  d'un message.
 
   removeParticipants(contactAccount: ContactAccount): Bluebird.Thenable<Discussion>;
-  //  Enleve le participant "contactAccount" de la discussion
+  //  Enleve le participant "contactAccount" de la Discussion
   //  courante. Plus exactement, supprime "contactAccount" d'un
-  //  GroupAccount de this.participants et l'evince du groupe de
+  //  GroupAccount de this.subdiscussion et l'evince du groupe de
   //  chat et de la conversation du cote du service (via un ConnectedApi).
   //  A noter que si jamais "contactAccount" etait present
   //  dans plusieurs GroupAccounts de this.participants, il ne
   //  sera supprime qu'une seule fois.
+	//  Si cela supprime le dernier participant d'une sous discussion,
+	//  la sous discussion sera supprimee.
 
-  getParticipants(): Bluebird.Thenable<GroupAccount[]>;
-  //  Retourne une liste des participants de la discussion courante.
+  getSubdiscussions(): Bluebird.Thenable<Subdiscussion>;
+  //  Retourne une liste des sous discussions de la Discussion courante.
 
-  getName(): Bluebird.Thenable<string>;
+  getName(): string;
   //  Retourne le nom de la discussion.
 
-  getDescription(): Bluebird.Thenable<string>;
+  getDescription(): string;
   //  Retourne une description de la discussion.
-
-  getSettings(): Bluebird.Thenable<utils.Dictionary<any>>;
-  //  Retourne tout les paramètres de la discussion, même spécifiques (map).
-  //  Bien evidemment, nous ne pourrons pas tout traiter.
-  //  Nous essayerons cependant de faire du mieux possible sans pour autant
-  //  y passer des heures entieres.
 }
