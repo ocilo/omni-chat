1dcf0cafb75855a8dba4c16d9ec5a1fb4b164043
Sn0wFox <ruben.pericas@laposte.net>
2016-04-22 13:07:56 +0200

    Updated OChatUser

--- src/user.ts
+++ src/user.ts
@@ -1,31 +1,34 @@
 import * as Bluebird from "bluebird";
 import {Contact} from "./interfaces/contact";
+import {ContactAccount} from "./interfaces/contact-account";
 import {User} from "./interfaces/user";
 import {Discussion} from "./interfaces/discussion";
 import {OChatContact} from "./contact";
 import {UserAccount} from "palantiri-interfaces";
-import {GroupAccount} from "palantiri-interfaces";
-import {EventEmitter} from "events";
+import {Message} from "palantiri-interfaces";
 
-export class OChatUser extends EventEmitter implements User {
+export class OChatUser implements User {
   username: string;
 
   accounts: UserAccount[] = [];
 
-  getOrCreateDiscussion(accounts: GroupAccount[]): Bluebird<Discussion> {
-    let discussion: Discussion; // The discussion we are looking for
-
-    // TODO : Oups, we have forgotten something.
-    //        There's three different cases :
-    //        * accounts contains only one account : easy as pie
-    //        * accounts contains several accounts but using the same protocol : easy
-    //        * accounts contains severas accounts using different protocols : the oups is here
-    //        What do we do here ? Trying to merge several discussions from differents protocols,
-    //        or using our own database to store these discussions ?
-    // NB : now we just have to use Proxy in each account this.accounts to get discussions.
-    //      That's a solved problem.
+  getOrCreateDiscussion(contactAccount: ContactAccount): Bluebird<Discussion> {
+    let discussion: Discussion = null; // The discussion we are looking for
+	  // TODO
     return Bluebird.resolve(discussion);
   }
+
+	getAllDiscussions(filter?: (discussion: Discussion) => boolean): Bluebird.Thenable<Discussion[]> {
+		let discussions: Discussion[] = []; // The discussions we are looking for
+		// TODO
+		return Bluebird.resolve(discussions);
+	}
+
+	getHeterogeneousDiscussions(filter?: (discussion: Discussion) => boolean): Bluebird.Thenable<Discussion[]> {
+		let discussions: Discussion[] = []; // The discussions we are looking for
+		// TODO : access db
+		return Bluebird.resolve(discussions);
+	}
 
   leaveDiscussion(discussion: Discussion): Bluebird.Thenable<User> {
 	  // TODO : two ways to implements this :
@@ -33,6 +36,73 @@ export class OChatUser extends EventEmitter implements User {
 	  //        -> just emit and event and connections will catch it and do what they need to do.
 		return Bluebird.resolve(this);
   }
+
+	sendMessage(msg: Message, discussion: Discussion, callback?: (err: Error) => any): Bluebird.Thenable<User> {
+		// TODO
+		return Bluebird.resolve(this);
+	}
+
+	getContacts(): Bluebird<Contact[]> {
+		// TODO : we can do lots of improvements :
+		//        -use the mail address(a lot more efficient !)
+		//        -do not compare otherContact with other someContacts we already added to contacts
+		//        -improve how we check if ContactAccount are the same Contact
+		//        -check in base if the user specified some time ago that some accounts are the same
+		let contacts: Contact[] = null;
+		for(let account of this.accounts) {
+			account.getContacts().then((someContacts) => {
+				let othersContacts: Contact[] = [];
+				for(let someContact of someContacts) {
+					let ctc = new OChatContact();
+					ctc.fullname = someContact.contactName;
+					if(!ctc.nicknames) {
+						ctc.nicknames = [];
+					}
+					if(!ctc.accounts) {
+						ctc.accounts = [];
+					}
+					ctc.nicknames.push(ctc.fullname);
+					ctc.accounts.push(someContact);
+					othersContacts.push(ctc);
+				}
+				if(!contacts) {
+					contacts = othersContacts;
+				} else {
+					for(let otherContact of othersContacts) {
+						let merge: boolean = false;
+						for(let actualContact of contacts) {
+							if(otherContact.fullname === actualContact.fullname) {
+								actualContact.mergeContacts(otherContact);
+								merge = true;
+								break;
+							}
+						}
+						if(!merge) {
+							contacts.push(otherContact);
+						}
+					}
+				}
+			})
+		}
+		return Bluebird.resolve(contacts);
+	}
+	addContact(contact: Contact, callback?: (err: Error, succes: Contact[]) => any): Bluebird.Thenable<User> {
+		// TODO : this is advanced option.
+		//        It's about writing on an account,
+		//        and not only reading it.
+		//        We will do this later.
+		return Bluebird.resolve(this);
+	}
+
+	removeContact(contact: Contact, callback?: (err: Error, succes: Contact[]) => any): Bluebird.Thenable<User> {
+		// WARNING : we need to warn the user that this will remove the contact from all his accounts
+		// TODO : this is advanced option.
+		//        It's about writing on an account,
+		//        and not only reading it.
+		//        We will do this later.
+		return Bluebird.resolve(this);
+	}
+
 
   getAccounts(protocols?: string[]): Bluebird<UserAccount[]> {
     if(protocols) {
@@ -48,50 +118,6 @@ export class OChatUser extends EventEmitter implements User {
       return Bluebird.resolve(accounts);
     }
     return Bluebird.resolve(this.accounts);
-  }
-
-  getContacts(): Bluebird<Contact[]> {
-    // TODO : we can do lots of improvements :
-    //        -do not compare otherContact with other someContacts we already added to contacts
-    //        -improve how we check if ContactAccount are the same Contact
-    //        -check in base if the user specified some time ago that some accounts are the same
-    let contacts: Contact[] = null;
-    for(let account of this.accounts) {
-      account.getContacts().then((someContacts) => {
-	      let othersContacts: Contact[] = [];
-	      for(let someContact of someContacts) {
-		      let ctc = new OChatContact();
-		      ctc.fullname = someContact.contactName;
-		      if(!ctc.nicknames) {
-			      ctc.nicknames = [];
-		      }
-		      if(!ctc.accounts) {
-			      ctc.accounts = [];
-		      }
-		      ctc.nicknames.push(ctc.fullname);
-		      ctc.accounts.push(someContact);
-		      othersContacts.push(ctc);
-	      }
-        if(!contacts) {
-          contacts = othersContacts;
-        } else {
-          for(let otherContact of othersContacts) {
-            let merge: boolean = false;
-            for(let actualContact of contacts) {
-              if(otherContact.fullname === actualContact.fullname) {
-                actualContact.mergeContacts(otherContact);
-                merge = true;
-                break;
-              }
-            }
-            if(!merge) {
-              contacts.push(otherContact);
-            }
-          }
-        }
-      })
-    }
-    return Bluebird.resolve(contacts);
   }
 
   addAccount(account: UserAccount, callback?: (err: Error, succes: UserAccount[]) => any): Bluebird.Thenable<User> {
@@ -122,61 +148,22 @@ export class OChatUser extends EventEmitter implements User {
 	  return Bluebird.resolve(this);
   }
 
-  addContact(contact: Contact, callback?: (err: Error, succes: Contact[]) => any): Bluebird.Thenable<User> {
-    // TODO : this is advanced option.
-    //        It's about writing on an account,
-    //        and not only reading it.
-    //        We will do this later.
-	  return Bluebird.resolve(this);
-  }
-
-  removeContact(contact: Contact, callback?: (err: Error, succes: Contact[]) => any): Bluebird.Thenable<User> {
-    // WARNING : we need to warn the user that this will remove the contact from all his accounts
-    // TODO : this is advanced option.
-    //        It's about writing on an account,
-    //        and not only reading it.
-    //        We will do this later.
-	  return Bluebird.resolve(this);
-  }
-
-	connectionsOn(event: string, handler: (...args: any[]) => any): Bluebird.Thenable<User> {
+	on(eventname: string, handler: (...args: any[]) => any): Bluebird.Thenable<User> {
 		for(let account of this.accounts) {
 			if(account.connection && account.connection.connected) {
 				account.getOrCreateConnection().then((co) => {
-					co.on(event, handler);
+					co.on(eventname, handler);
 				})
 			}
 		}
 		return Bluebird.resolve(this);
 	}
 
-	connectionsOnce(event: string, handler: (...args: any[]) => any): Bluebird.Thenable<User> {
+	once(eventname: string, handler: (...args: any[]) => any): Bluebird.Thenable<User> {
 		for(let account of this.accounts) {
 			if(account.connection && account.connection.connected) {
 				account.getOrCreateConnection().then((co) => {
-					co.once(event, handler);
-				})
-			}
-		}
-		return Bluebird.resolve(this);
-	}
-
-	removeConnectionsListener(event: string, handler: (...args: any[]) => any): Bluebird.Thenable<User> {
-		for(let account of this.accounts) {
-			if(account.connection && account.connection.connected) {
-				account.getOrCreateConnection().then((co) => {
-					co.removeListener(event, handler);
-				})
-			}
-		}
-		return Bluebird.resolve(this);
-	}
-
-	connectionsSetMaxListeners(n: number): Bluebird.Thenable<User> {
-		for(let account of this.accounts) {
-			if(account.connection && account.connection.connected) {
-				account.getOrCreateConnection().then((co) => {
-					co.setMaxListeners(n);
+					co.once(eventname, handler);
 				})
 			}
 		}
