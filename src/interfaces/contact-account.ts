9cfb0e211d0a70e7e0a03a5268a3da232331909e
Sn0wFox <ruben.pericas@laposte.net>
2016-04-22 11:34:37 +0200

    Added type ContactAccount

--- src/interfaces/contact-account.ts
+++ src/interfaces/contact-account.ts
@@ -1,3 +1,11 @@
 /**
  * Created by Ruben on 22/04/2016.
  */
+import {Contact} from "palantiri-interfaces";
+
+/***************************************************************
+ * ContactAccount represents a mono-protocol Contact, reachable
+ * through one UserAccuont.
+ * The type alias prevent some misunderstandings.
+ ***************************************************************/
+export type ContactAccount = Contact;
