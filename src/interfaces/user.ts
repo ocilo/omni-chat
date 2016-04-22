cb61cd9ec4217ef1f7089de4efbc5cfa7b8f82ed
Sn0wFox <ruben.pericas@laposte.net>
2016-04-22 12:48:11 +0200

    Updated interface User

--- src/interfaces/user.ts
+++ src/interfaces/user.ts
@@ -1,77 +1,88 @@
 import * as Bluebird from "bluebird";
-import {UserAccount} from "palantiri-interfaces";
-import {GroupAccount} from "palantiri-interfaces";
 import {Contact} from "./contact";
 import {Discussion} from "./discussion";
-import {EventEmitter} from "events";
+import {ContactAccount} from "./contact-account";
+import {UserAccount} from "palantiri-interfaces";
+import {Message} from "palantiri-interfaces";
 
 /***************************************************************
  * User is the representation of someone connected with OmniChat.
- * An user works quite like a Contact : you just have more
- * rights as an user (for example acceed to your own contacts).
+ * It allows you to acceed to yout own Accounts, acceed to your
+ * own Contacts, create Discussions with them, and so on.
  ***************************************************************/
-export interface User extends EventEmitter{
-  accounts: UserAccount[];  //  La liste des comptes connus de l'utilisateur
+export interface User {
+  accounts: UserAccount[];  //  La liste des comptes connus de l'utilisateur.
 
-  username: string;         //  Le nom complet de l'utilisateur
+  username: string;         //  Le nom complet de l'utilisateur.
 
-  getOrCreateDiscussion(accounts: GroupAccount[]) : Bluebird.Thenable<Discussion>;
-  //  Permet de commencer une discussion avec un contact,
-  //  ou de recuperer une discussion existante.
-  //  C'est le seul moyen de communiquer avec quelqu'un.
-  //  En cas de création, garanti que l'initiateur de la
-  //  conversation est present en tant que participant.
+  getOrCreateDiscussion(contactAccount: ContactAccount): Bluebird.Thenable<Discussion>;
+  //  Permet de commencer une discussion avec un contact ou
+	//  de recuperer une discussion existante, sur un compte
+	//  utilisant un protocol specifique.
 
-  leaveDiscussion(discussion: Discussion): Bluebird.Thenable<User>;
-  //  Permet de quitter la discussion "discussion" et de ne plus
-  //  recevoir les notifications associées.
+	getAllDiscussions(filter?: (discussion: Discussion) => boolean): Bluebird.Thenable<Discussion[]>;
+	//  Permet de recuperer toutes les Discussions de l'utilisateur :
+	//  Celles pour chacun de ces comptes.
+	//  Celles heterogenes.
+	//  Si "filter" est precise, ne retourne que les Discussions pour
+	//  lesquelles "filter" retourne vrai.
+
+	getHeterogeneousDiscussions(filter?: (discussion: Discussion) => boolean): Bluebird.Thenable<Discussion[]>;
+	//  Retourne toutes les Discussions de l'utilisateur courant
+	//  qui sont heterogenes.
+	//  Cette methode est plus rapide que d'appeler getAllDiscussions
+	//  et de filtrer les Discussions heterogenes.
+	//  Si "filter" est precise, ne retourne que les Discussions pour
+	//  lesquelles "filter" retourne vrai.
+
+	leaveDiscussion(discussion: Discussion): Bluebird.Thenable<User>;
+	//  Permet de quitter la Discussion "discussion" et de ne plus
+	//  recevoir les notifications associées.
+
+	sendMessage(msg: Message, discussion: Discussion, callback?: (err: Error) => any): Bluebird.Thenable<User>;
+	//  Envoie le Message "msg" dans la Discussion "discussion".
+	//  Si un des participants a la Discussion ne peut pas recevoir
+	//  le Message, "err" sera non nul.
+	//  A noter qu'un message textuel sera toujours envoye,
+	//  meme si le protocole utilise par le compte ne supporte pas
+	//  le type de Message.
+
+	getContacts(filter?: (contact: Contact) => boolean): Bluebird.Thenable<Contact[]>;
+	//  Retourne la liste des contacts de l'utilisateur courant,
+	//  c'est a dire ceux de tous ses comptes, et fusionnes
+	//  lorsqu'il est avere que les comptes representent la
+	//  meme personne.
 
   getAccounts(protocols?: string[]): Bluebird.Thenable<UserAccount[]>;
   //  Retourne la liste des comptes de l'utilisateurs.
   //  Si "protocol" est precise, ne retourne que la lite des
   //  comptes de l'utilisateur courant qui utilise le
   //  protocole "protocol".
-  //  Ce comptes peuvent bien entendu etre de tout type :
-  //  IRC, Skype, Facebook... mais aussi OmniChat (recursivite).
-
-  getContacts(): Bluebird.Thenable<Contact[]>;
-  //  Retourne la liste des contacts de l'utilisateur courant.
-  //  Pour chaque compte lie a l'utilisateur,
 
   addAccount(account: UserAccount, callback? : (err: Error, succes: UserAccount[]) => any): Bluebird.Thenable<User>;
-  //  Ajoute un compte a l'utilisateur courant
+  //  Ajoute un compte a l'utilisateur courant.
 
   removeAccount(account: UserAccount, callback? : (err: Error, succes: UserAccount[]) => any): Bluebird.Thenable<User>;
-  //  Supprime un compte de l'utilisateur courant
+  //  Supprime un compte de l'utilisateur courant.
 
   addContact(contact: Contact, callback? : (err: Error, succes: Contact[]) => any): Bluebird.Thenable<User>;
-  //  Ajoute un contact a l'utilisateur courant
+  //  Ajoute un contact a l'utilisateur courant.
 
   removeContact(contact: Contact, callback?: (err: Error, succes: Contact[]) => any): Bluebird.Thenable<User>;
-  //  Supprime un contact de l'utilisateur courant
+  //  Supprime un contact de l'utilisateur courant.
 
-	connectionsOn(event: string, handler: (...args: any[]) => any): Bluebird.Thenable<User>;
-	//  Allows all current Connections of all accounts to react
-	//  to the event "event" by triggering the function "handler".
-	//  Note that this will take effect only on the connections
-	//  already established.
+	on(eventname: string, handler: (...args: any[]) => any): Bluebird.Thenable<User>;
+	//  Permet d'enregistrer l'utilisateur courant en tant que
+	//  consommateur de l'Event "eventname", envoye par les
+	//  differentes Connections de ses comptes.
+	//  A chaque fois que l'utilisateur courant recevra l'Event
+	//  "eventname", il déclenchera la fonction "handler".
 
-	connectionsOnce(event: string, handler: (...args: any[]) => any): Bluebird.Thenable<User>;
-	//  Allows all current Connections of all accounts to react
-	//  to the event "event" by triggering the function "handler",
-	//  but only once.
-	//  Note that this will take effect only on the connections
-	//  already established.
-
-	removeConnectionsListener(event: string, handler: (...args: any[]) => any): Bluebird.Thenable<User>;
-	//  Remove all current listeners for the event "event" which
-	//  trigger "handler" of each current connections of all accounts.
-	//  Note that this will take effect only on the connections
-	//  already established.
-
-	connectionsSetMaxListeners(n: number): Bluebird.Thenable<User>;
-	//  Set the maximum number of listeners that each Connection
-	//  of all accounts could use.
-	//  Note that this will take effect only on the connections
-	//  already established.
+	once(eventname: string, handler: (...args: any[]) => any): Bluebird.Thenable<User>;
+	//  Permet d'enregistrer l'utilisateur courant en tant que
+	//  consommateur de l'Event "eventname", envoye par les
+	//  differentes Connections de ses comptes.
+	//  Lorsque l'utilisateur courant recevra l'Event "eventname",
+	//  il déclenchera une seule fois la fonction "handler",
+	//  puis ne surveillera plis l'Event "eventname".
 }
