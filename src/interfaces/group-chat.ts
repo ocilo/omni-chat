28ccb0c98a874e9bd3d4952044c7554219e01d26
Sn0wFox <ruben.pericas@laposte.net>
2016-04-22 10:40:23 +0200

    Added type alias GroupChat for palantiri.Discussion

---
+++ src/interfaces/group-chat.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by Ruben on 22/04/2016.
+ */
+import {Discussion} from "palantiri-interfaces";
+
+/***************************************************************
+ * GroupChat represents a mono-protocol and mono-account
+ * Discussion. This allows us to keep the aspect of "group chat"
+ * in the side of the protocol used by Palantiri.
+ * The type alias prevent some misunderstandings.
+ ***************************************************************/
+export type GroupChat = Discussion;
