a504ee5e34598fdcffc862c6a77fd20623766cb7
Sn0wFox <ruben.pericas@laposte.net>
2016-05-09 13:54:51 +0200

    Splited App in App and DriversStore

--- src/interfaces/app.ts
+++ src/interfaces/app.ts
@@ -1,48 +1,11 @@
-import * as palantiri from "palantiri-interfaces";
-import {Thenable} from "bluebird";
 import {UserInterface} from "./user";
-import {UserAccountInterface} from "./user-account";
 
 /***************************************************************
  * App is the entry point for the library.
  * It maintains the list of connected users.
  ***************************************************************/
-export type ConnectionStrategy = (account: UserAccountInterface) => palantiri.Connection | Thenable <palantiri.Connection>;
-
-// Idea:
-// Rename it to "drivers-store" and only handle the palantiri-API acquisition (available drivers and active connections)
-
 export interface AppInterface {
   /**
-   * Register a new driver with its data acquisition function
-   */
-  useDriver (driver: palantiri.Connection.Constructor<any, any>, strategy: ConnectionStrategy): AppInterface;
-
-  /**
-   * Add this connection to the set of active connections.
-   */
-  addActiveConnection (account: UserAccountInterface, connection: palantiri.Connection): Thenable<AppInterface>;
-
-	/**
-   * Look for a Connection for the given account.
-   * If no one exists, return a null reference.
-   */
-  getConnection (account: UserAccountInterface): Thenable<palantiri.Connection>;
-
-	/**
-   * Look for a Connection for the given account.
-   * If no one exists, try to instanciate one and then return it.
-   */
-  getOrCreateConnection (account: UserAccountInterface): Thenable<palantiri.Connection>;
-
-  /**
-   * Look for an Api for the given account.
-   * If the Account has no Connection, try to create one.
-   * If the Connection has no Api yet, try to create one then return it.
-   */
-  getOrCreateApi (account: UserAccountInterface): Thenable<palantiri.Api>;
-
-	/**
    * Return the list of connected Users.
    * If filter is specified, return only the Users for chich
    * filter returns true.
---
+++ src/interfaces/drivers-store.ts
@@ -0,0 +1,48 @@
+import * as palantiri from "palantiri-interfaces";
+import {UserAccountInterface} from "./user-account";
+import {AppInterface} from "./app";
+import {Thenable} from "bluebird";
+
+/**
+ * The type of function we must use to connect an account.
+ * Most of the time it will be done the asynchronous way.
+ */
+export type ConnectionStrategy = (account: UserAccountInterface) => palantiri.Connection | Thenable <palantiri.Connection>;
+
+/**
+ * DriversStore is a big tool that allows us to maintain the
+ * list of connections and drivers curently used.
+ * It's mostly useful servor side, and is made to be static.
+ */
+export interface DriversStoreInterface {
+  /**
+   * Register a new driver with its data acquisition function
+   */
+  useDriver (driver: palantiri.Connection.Constructor<any, any>, strategy: ConnectionStrategy): AppInterface;
+
+  /**
+   * Add this connection to the set of active connections.
+   */
+  addActiveConnection (account: UserAccountInterface, connection: palantiri.Connection): Thenable<AppInterface>;
+
+  /**
+   * Look for a Connection for the given account.
+   * If no one exists, return a null reference.
+   */
+  getConnection (account: UserAccountInterface): Thenable<palantiri.Connection>;
+
+  /**
+   * Look for a Connection for the given account.
+   * If no one exists, try to instanciate one and then return it.
+   */
+  getOrCreateConnection (account: UserAccountInterface): Thenable<palantiri.Connection>;
+
+  /**
+   * Look for an Api for the given account.
+   * If the Account has no Connection, try to create one.
+   * If the Connection has no Api yet, try to create one then return it.
+   */
+  getOrCreateApi (account: UserAccountInterface): Thenable<palantiri.Api>;
+}
+
+export default DriversStoreInterface;
