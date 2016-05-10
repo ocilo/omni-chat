b2a04f8c0c1e2b549111e63e1ba99e2e00126b0c
Sn0wFox <ruben.pericas@laposte.net>
2016-05-10 17:38:59 +0200

    Added first implementation for SimpleMessage

--- src/simple-message.ts
+++ src/simple-message.ts
@@ -3,36 +3,78 @@ import * as Bluebird from "bluebird";
 import * as palantiri from "palantiri-interfaces";
 import {UserAccountInterface} from "./interfaces/user-account";
 import {ContactAccountInterface} from "./interfaces/contact-account";
-import Incident from "incident";
 
 /**
  * Represents a high-level message (wrap passive message objects in accessors).
  */
 export class SimpleMessage implements MessageInterface {
-  messageData: palantiri.Message;
+	/**
+   * The low-level message from palantiri.
+   */
+  protected messageData: palantiri.Message;
+
+	/**
+   * The author of the current message.
+   */
+  protected author: UserAccountInterface | ContactAccountInterface;
+
+	/**
+   * True only if the current message was already delivered.
+   */
+  protected delivered: boolean;
+  // TODO: the previous two attributes are maybe redondant with properties in messageData.
 
   constructor(messageData: palantiri.Message) {
     this.messageData = messageData;
+    this.author = null;
+    this.delivered = false;
   }
 
+  /* MessageInterface implementation*/
+  /**
+   * Return the body of the current Message.
+   */
   getBody (): Bluebird<string> {
     return Bluebird.resolve(this.messageData.body);
   }
 
-  getAuthorAccount (): Bluebird<UserAccountInterface | ContactAccountInterface> {
-    return Bluebird.reject(new Incident("todo", "SimpleMessage:getAuthorAccount is not implemented yet"));
-  }
-
+  /**
+   * Return true only if this message is delivered.
+   * For simple-messages, it means that the service acknowledged the reception of the message.
+   */
   isDelivered(): Bluebird<boolean> {
-    return Bluebird.reject(new Incident("todo", "SimpleMessage:isDelivered is not implemented yet"));
+    return Bluebird.resolve(this.delivered);
   }
 
+  /**
+   * Return the date of last editing.
+   */
   getLastEditingDate(): Bluebird<Date> {
     return Bluebird.resolve(this.messageData.lastUpdated);
   }
 
+  /**
+   * Return the date of the current Message creation.
+   */
   getCreationDate (): Bluebird<Date> {
     return Bluebird.resolve(this.messageData.creationDate);
+  }
+
+  /* Specific methods */
+	/**
+   * Return the author of the current message.
+   */
+  getAuthorAccount (): Bluebird<UserAccountInterface | ContactAccountInterface> {
+    return Bluebird.resolve(this.author);
+  }
+
+	/**
+   * Specify that the current message has been delivered.
+   * @returns {SimpleMessage}
+   */
+  setDelivered (): SimpleMessage {
+    this.delivered = true;
+    return this;
   }
 }
 
